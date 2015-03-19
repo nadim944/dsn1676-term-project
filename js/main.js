1c7566dabc5f8695bada94649054be99a6189a08
nadim944 <nadeem944@hotmail.com>
2015-03-19 14:34:36 -0400

    Fixed button animation
    Fixed button animation for playbtn2 and bounce for reverse button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $navBtn = $('.nav-btn');
 var $panel = $('.panel');
 var $reverseBtn = $('.reverse-btn');
-var $playBtn2 = $('play-btn2')
+var $playBtn2 = $('.play-btn2')
 
 $navBtn.on('click', function () {
     $panel.toggleClass('js-panel-open');
@@ -11,8 +11,6 @@ $reverseBtn.on('click', function () {
     $reverseBtn.toggleClass('bounce');
 });
 
-$playBtn2
-
-$('.box img').on('click', function() {
-    $(this).toggleClass('clicked');
-});
+$playBtn2.on('click', function () {
+    $playBtn2.toggleClass('shake');
+    });
