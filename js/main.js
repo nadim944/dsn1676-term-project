85a74a0056626ab9eacd2c7c916aaef9185945d4
nadim944 <nadeem944@hotmail.com>
2015-03-19 11:29:34 -0400

    Fixed toggle error and added shake effect
    Fixed toggle issue, and added shake effect to play button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var $navBtn = $('.nav-btn');
 var $panel = $('.panel');
-
 var $reverseBtn = $('.reverse-btn');
-
+var $playBtn2 = $('play-btn2')
 
 $navBtn.on('click', function () {
     $panel.toggleClass('js-panel-open');
 });
 
-var $reverse-btn = $('.reverse-btn');
-
 $reverseBtn.on('click', function () {
     $reverseBtn.toggleClass('bounce');
+});
+
+$playBtn2
+
+$('.box img').on('click', function() {
+    $(this).toggleClass('clicked');
 });
