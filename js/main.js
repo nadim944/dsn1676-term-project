5e9726a579e3c974de5902491b292cb5cc620f91
nadim944 <nadeem944@hotmail.com>
2015-03-18 23:08:00 -0400

    Added key frames to buttons and resized
    Added 3 music buttons and added key frames for bounce animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var $navBtn = $('.nav-btn');
 var $panel = $('.panel');
 
+var $reverseBtn = $('.reverse-btn');
+
+
 $navBtn.on('click', function () {
     $panel.toggleClass('js-panel-open');
+});
 
+var $reverse-btn = $('.reverse-btn');
+
+$reverseBtn.on('click', function () {
+    $reverseBtn.toggleClass('bounce');
 });
